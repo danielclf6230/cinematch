ac4fd1e2ff3ebdc2d1dc240bd4157a692f820e68
ericjyr <ericjyr@gmail.com>
2023-09-20 16:14:08 -0600

    Initial commit

---
+++ cinematch-app/src/test/java/com/cm/cinematchapp/CinematchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.cinematchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
