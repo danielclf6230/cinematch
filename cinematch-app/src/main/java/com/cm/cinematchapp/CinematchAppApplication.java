029aa8cf2d4b7745daaf0254acf5712a8f9a852f
ericjyr <ericjyr@gmail.com>
2023-09-20 16:03:08 -0600

    Initial Commit

---
+++ cinematch-app/src/main/java/com/cm/cinematchapp/CinematchAppApplication.java
@@ -0,0 +1,13 @@
+package com.cm.cinematchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematchAppApplication.class, args);
+	}
+
+}
