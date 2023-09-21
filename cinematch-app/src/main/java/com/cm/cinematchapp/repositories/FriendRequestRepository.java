e8ce732486ea47f1371def4c1cb3ee4d60f01643
ericjyr <ericjyr@gmail.com>
2023-09-21 15:54:44 -0600

    Updates made to class

--- cinematch-app/src/main/java/com/cm/cinematchapp/repositories/FriendRequestRepository.java
+++ cinematch-app/src/main/java/com/cm/cinematchapp/repositories/FriendRequestRepository.java
@@ -2,8 +2,14 @@ package com.cm.cinematchapp.repositories;
 
 import com.cm.cinematchapp.entities.FriendRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+
+    @Query("SELECT fr FROM FriendRequest fr WHERE fr.user.userId = :userId")
+    List<FriendRequest> getFriendRequestsByUserId(Long userId);
 }
--- cinematch-app/src/main/java/com/cm/cinematchapp/services/FriendRequestService.java
+++ cinematch-app/src/main/java/com/cm/cinematchapp/services/FriendRequestService.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class FriendRequestService {
@@ -19,25 +21,31 @@ public class FriendRequestService {
     @Autowired
     private FriendshipRepository friendshipRepository;
 
-    public void acceptFriendRequest(Long friendRequestId) {
-        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId).orElse(null);
 
-        if (friendRequest != null) {
-            if (friendRequest.getRequestStatus() == FriendRequest.FriendRequestStatus.PENDING) {
-                // Update the request status to "Accepted"
-                friendRequest.setRequestStatus(FriendRequest.FriendRequestStatus.ACCEPTED);
-                friendRequestRepository.save(friendRequest);
-
-                // Create an entry in the Friendship table
-                Friendship friendship = new Friendship();
-                friendship.setUserId(friendRequest.getRequester().getUserId());
-                friendship.setFriendUserId(friendRequest.getUser().getUserId());
-                friendshipRepository.save(friendship);
-            } else {
-                // Request is already accepted or rejected, remove it from the table
-                friendRequestRepository.delete(friendRequest);
-            }
-        }
+    public List<FriendRequest> getFriendRequestsByUserId(Long userId) {
+        return friendRequestRepository.getFriendRequestsByUserId(userId);
     }
+
+//    //accepting friend req.
+//    public void acceptFriendRequest(Long friendRequestId) {
+//        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId).orElse(null);
+//
+//        if (friendRequest != null) {
+//            if (friendRequest.getRequestStatus() == FriendRequest.FriendRequestStatus.PENDING) {
+//                // Update the request status to "Accepted"
+//                friendRequest.setRequestStatus(FriendRequest.FriendRequestStatus.ACCEPTED);
+//                friendRequestRepository.save(friendRequest);
+//
+//                // Create an entry in the Friendship table
+//                Friendship friendship = new Friendship();
+//                friendship.setUserId(friendRequest.getRequester().getUserId());
+//                friendship.setFriendUserId(friendRequest.getUser().getUserId());
+//                friendshipRepository.save(friendship);
+//            } else {
+//                // Request is already accepted or rejected, remove it from the table
+//                friendRequestRepository.delete(friendRequest);
+//            }
+//        }
+//    }
 
 }
