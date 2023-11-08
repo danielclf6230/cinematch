795fcf174ba3f7f8c1de5070d26043a7543af16a
JR Ventura <jrventura.9@gmail.com>
2023-11-07 17:38:46 -0700

    Added LOGO to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from './/res/logo_v1.png'; // Adjust the path as needed
+
 
 class Header extends Component {
     constructor(props) {
@@ -11,16 +13,17 @@ class Header extends Component {
         return (
             <div>
                 <header>
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                        <div className="menu-ops">
+                    <nav className="row navbar navbar-expand-md navbar-dark bg-dark">
+                        <div className="menu-ops col">
                             <Link to="/home" className="navbar-brand">home</Link>
                             <Link to="/register" className="navbar-brand">register</Link>
                             <Link to="/" className="navbar-brand">login</Link>
                             <Link to="/swipe" className="navbar-brand">swipe</Link>
                         </div>
-                        <div className="cm-logo">
-
+                        <div className="cm-logo col">
+                            <img src={logo} alt="Cinematch" />
                         </div>
+                        <div className="col"></div>
                     </nav>
                 </header>
             </div>
