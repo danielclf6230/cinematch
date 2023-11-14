1b3dc86d00db3d5371c9e28701de0dfaa7e539bc
JR Ventura <jrventura.9@gmail.com>
2023-11-14 15:58:18 -0700

    updating room css for demo

--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -4,17 +4,17 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3001');
 
 function Room() {
-    // const [message, setMessage] = useState("");
-    // const [messageRecevied, setMessageRecevied] = useState("");
-    // const sendMessage = () =>{
-    // socket.emit("send_message", {message});
-    // };
-    //
-    // useEffect(()=>{
-    // socket.on("receive_message",(data)=>{
-    //     setMessageRecevied(data.message);
-    // })
-    // },[socket])
+    const [message, setMessage] = useState("");
+    const [messageRecevied, setMessageRecevied] = useState("");
+    const sendMessage = () =>{
+    socket.emit("send_message", {message});
+    };
+
+    useEffect(()=>{
+    socket.on("receive_message",(data)=>{
+        setMessageRecevied(data.message);
+    })
+    },[socket])
     //
     // return (
     //     <div className={"Room"}>
@@ -79,25 +79,38 @@ function Room() {
     };
 
     return (
-      <div className="App">
-       <h1>User: Enter a Number</h1>
-        <input
-          type="number"
-          value={numberInput}
-          onChange={handleInputChange}
-          placeholder="Enter a number"
-        />
-        <input
+      <div className="Room container text-center">
+          <div>
+              <h1>User: Enter a Number</h1>
+          </div>
+        {/*<input*/}
+        {/*  type="number"*/}
+        {/*  value={numberInput}*/}
+        {/*  onChange={handleInputChange}*/}
+        {/*  placeholder="PLACEHOLDER FOR SWIPING"*/}
+        {/*/>*/}
+          <input
           type="text"
           value={room}
           onChange={handleRoomChange}
           placeholder="Enter Room Number"
         />
-        <button onClick={handleJoinRoom}>Join Room</button>
-        <button onClick={handleChooseNumber}>Choose Number</button>
+          <button onClick={handleJoinRoom}>Join Room</button>
+          {/*<button onClick={handleChooseNumber}>Choose Number</button>*/}
         {waiting && <p>Waiting for the other user to choose a number.</p>}
         {result && <p>{result}</p>}
+
+          <div className="Room">
+              <input placeholder="Message..." onChange={(event) =>{
+                  setMessage(event.target.value)
+              }}/>
+              <button onClick={sendMessage}>Send Message</button>
+              <h1>Message:
+                  {messageRecevied}</h1>
+          </div>
+
       </div>
+
     );
 }
 
