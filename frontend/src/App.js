536f43da55b56075fdb9968b2ee09ad15dcb2eb1
JR Ventura <jrventura.9@gmail.com>
2023-11-20 02:03:04 -0700

    working on pulling FULL list of movies from DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import Home from './components/Home'
 import Swipe from './components/Swipe'
 import Admin from './components/Admin'
 import Room from "./components/Room"
-import Search from "./components/Search"
 import MovieSearch from "./components/MovieSearch";
 
 
