3180f6ed230a5d038f759021753671267ee29bb8
danielclf6230 <lingfung1230888@gmail.com>
2023-11-25 12:39:47 -0700

    Friends page updated

--- frontend/src/components/Friends.jsx
+++ frontend/src/components/Friends.jsx
@@ -1,81 +1,48 @@
 // App.js
-import React, { useState, useEffect } from 'react';
-import FriendRequests from '../components/Friend/FriendRequests';
-import FriendList from '../components/Friend/FriendList';
-import {io} from "socket.io-client";
-
-
-const socket = new io('http://localhost:3001');
+import React, { useState} from 'react';
+import { entitiesApi } from '../api/entitiesApi';
+import SideMenu from "./SideMenu";
+import {actionsApi} from "../api/actionsApi";
 
 const Friends = () => {
-    const [friendRequests, setFriendRequests] = useState([]);
-    const [friendList, setFriendList] = useState([]);
-    const [searchUserId, setSearchUserId] = useState('');
+    const [searchUserName, setSearchUserName] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
 
-    const handleSearch = () => {
-        // Perform the search based on searchUserId
-        // For simplicity, assume the user with the entered Socket ID exists
-        sendFriendRequest(searchUserId);
-    };
-
-
-    useEffect(() => {
-        // Listen for friend requests
-        socket.on('friendRequest', (request) => {
-            setFriendRequests((prevRequests) => [...prevRequests, request]);
-        });
-
-        // Listen for accepted friend requests
-        socket.on('friendRequestAccepted', (friend) => {
-            setFriendList((prevList) => [...prevList, friend]);
-            setFriendRequests((prevRequests) =>
-                prevRequests.filter((request) => request.id !== friend.id)
-            );
-        });
-
-        return () => {
-            // Disconnect socket on component unmount
-            socket.disconnect();
-        };
-    }, []);
-
-    const sendFriendRequest = (friendId) => {
-        const request = { id: friendId, status: 'pending' };
-        socket.emit('sendFriendRequest', request);
-        setFriendRequests([...friendRequests, request]);
-    };
-
-    const acceptFriendRequest = (request) => {
-        socket.emit('acceptFriendRequest', request);
-        setFriendList([...friendList, request]);
-        setFriendRequests((prevRequests) =>
-            prevRequests.filter((r) => r.id !== request.id)
-        );
-    };
-
-    const rejectFriendRequest = (request) => {
-        socket.emit('rejectFriendRequest', request);
-        setFriendRequests((prevRequests) =>
-            prevRequests.filter((r) => r.id !== request.id)
-        );
+    const handleSearchUser = async () => {
+        try {
+            const response = await entitiesApi.getUsersByUsername(searchUserName);
+            console.log(response.data);
+            setSearchResult(response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
-        <div>
+        <div className="App">
+            <SideMenu/>
+            <div>
+                <h2>Search User</h2>
             <input
                 type="text"
-                placeholder="Enter User 2 Socket ID"
-                value={searchUserId}
-                onChange={(e) => setSearchUserId(e.target.value)}
+                placeholder="Enter username"
+                onChange={(e) => setSearchUserName(e.target.value)}
             />
-            <button onClick={handleSearch}>Send Friend Request</button>
+            <button onClick={handleSearchUser}>Search</button>
 
-            <FriendRequests
-                friendRequests={friendRequests}
-                onAccept={acceptFriendRequest}
-                onReject={rejectFriendRequest}
-            />
-            <FriendList friendList={friendList} />
+                <div>
+                    <h2>Friend List</h2>
+                    <ul>
+                        {searchResult.map((user) => (
+                            <li>
+                                <p>{`${user.username}`}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
 }
