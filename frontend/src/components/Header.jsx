b2a571082cc88800d0876ae390a864829f592781
JR Ventura <jrventura.9@gmail.com>
2023-11-30 14:31:34 -0700

    CSS changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './/res/logo_v1.png'; // Adjust the path as needed
+import logo from './res/logo_v1.png';
+import { useAuth,  handleLogError } from '../security/AuthContext';
 
+const Header = () => {
+    const { user, logout } = useAuth();
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="row navbar navbar-expand-md navbar-dark bg-dark">
-                        <div className="menu-ops col">
-                            <Link to="/home" className="navbar-brand">home</Link>
-                            <Link to="/register" className="navbar-brand">register</Link>
-                            <Link to="/" className="navbar-brand">login</Link>
-                            <Link to="/" className="navbar-brand">logout</Link>
-
-                        </div>
-                        <div className="cm-title col">
-                            <h1>CINEMATCH</h1>
-                        </div>
-                        <div className="cm-logo col-1">
-                            <img src={logo} alt="Cinematch Logo" />
-                        </div>
-                    </nav>
-                </header>
-            </div>
-        )}
-}
+    return (
+        <div>
+            <header>
+                <nav className="row navbar navbar-expand-md navbar-dark bg-dark">
+                    <div className="menu-ops col">
+                        <Link to={user ? '/home' : '/'} className="navbar-brand">
+                            home
+                        </Link>
+                        <Link to="/register" className="navbar-brand">
+                            register
+                        </Link>
+                        {user ? (
+                            <>
+                                <Link to="/" className="navbar-brand" onClick={logout}>
+                                    logout
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/" className="navbar-brand">
+                                    login
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                    <div className="cm-title col">
+                        <h1>CINEMATCH</h1>
+                    </div>
+                    <div className="cm-logo col-1">
+                        <img src={logo} alt="Cinematch Logo" />
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+};
 
 export default Header;
