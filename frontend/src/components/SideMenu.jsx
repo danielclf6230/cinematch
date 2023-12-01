a4a2137f168f66417a3931ab53109041de8bb90e
danielclf6230 <lingfung1230888@gmail.com>
2023-12-01 16:51:34 -0700

    Styling updated

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -23,12 +23,28 @@ class SideMenu extends Component {
 
             <div className="sideMenu">
                 <div>
-                    <Link to="/home" className="navbar-brand"><h2>Profile</h2></Link> <br/>
-                    <Link to="/swipe" className="navbar-brand"><h2>Swipe</h2></Link> <br/>
-                    <Link to="/room" className="navbar-brand"><h2>Group Swipe</h2></Link> <br/>
-                    <Link to="/friends" className="navbar-brand"><h2>Friends</h2></Link> <br/>
-                    {isAdmin && <Link to="/admin" className="navbar-brand"><h2>Search</h2></Link> }<br />
-                    {/*<Link to="/admin" className="navbar-brand"><h2>Search</h2></Link> <br/>*/}
+                    <Link to="/home" className="navbar-brand">
+                        <h2>Profile</h2>
+                    </Link>
+                    <br />
+                    <Link to="/swipe" className="navbar-brand">
+                        <h2>Swipe</h2>
+                    </Link>
+                    <br />
+                    <Link to="/room" className="navbar-brand">
+                        <h2>Group Swipe</h2>
+                    </Link>
+                    <br />
+                    <Link to="/friends" className="navbar-brand">
+                        <h2>Friends</h2>
+                    </Link>
+                    <br />
+                    {isAdmin && (
+                        <Link to="/admin" className="navbar-brand">
+                            <h2>Search</h2>
+                        </Link>
+                    )}
+                    <br />
                 </div>
             </div>
         )}
